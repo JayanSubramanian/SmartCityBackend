e3cb199358c7be7c8f15a24b12e5b93702a14b46
Jayan Subramanian <subramanianjayan226@gmail.com>
2025-01-22 21:48:37 +0530

    inital commit

---
+++ final_code.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+
+X_train_tensor = torch.load('X_train_tensor.pt')
+edge_index = torch.load('edge_index.pt')
+
+class GATWithDimensionalityReduction(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, reduce_dim, num_heads=4):
+        super(GATWithDimensionalityReduction, self).__init__()
+        self.conv1 = GATConv(in_channels, hidden_channels, heads=num_heads, edge_dim=1)
+        self.conv2 = GATConv(hidden_channels * num_heads, hidden_channels * 2, heads=num_heads, edge_dim=1)
+        self.conv3 = GATConv(hidden_channels * 2 * num_heads, hidden_channels * 4, heads=num_heads, edge_dim=1)
+        self.conv4 = GATConv(hidden_channels * 4 * num_heads, hidden_channels * 8, heads=num_heads, edge_dim=1)
+
+        self.dim_reduce = nn.Linear(hidden_channels * 8 * num_heads, reduce_dim)
+
+        self.fc1 = nn.Linear(reduce_dim, 64)
+        self.fc2 = nn.Linear(64, 1)
+
+    def forward(self, x, edge_index, edge_attr):
+        x = F.elu(self.conv1(x, edge_index))
+        x = F.elu(self.conv2(x, edge_index))
+        x = F.elu(self.conv3(x, edge_index))
+        x = F.elu(self.conv4(x, edge_index))
+
+        x = self.dim_reduce(x)
+        x = F.elu(x)
+
+        x = F.elu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+torch.manual_seed(42)
+
+in_channels = X_train_tensor.shape[1]
+hidden_channels = 8
+out_channels = 16
+reduce_dim = 32
+num_heads = 4
---
+++ main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, UploadFile, Query, HTTPException
+from fastapi.responses import JSONResponse
+import torch
+from final_code import GATWithDimensionalityReduction, in_channels, hidden_channels, out_channels, reduce_dim, num_heads
+from starlette.responses import RedirectResponse
+from engine import run_model
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+import overpy
+import os
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = GATWithDimensionalityReduction(in_channels=in_channels, hidden_channels=hidden_channels, out_channels=out_channels, reduce_dim=reduce_dim, num_heads=num_heads)
+model.load_state_dict(torch.load("GATWithDimensionalityReduction.pth", map_location=torch.device('cpu'), weights_only=True))
+model.eval()
+
+@app.post("/predict")
+async def predict(files: List[UploadFile]):
+    risk = await run_model(files)
+    response_data = {"risk": round(risk*100, 2)}
+
+    print(response_data)
+    return JSONResponse(content=response_data)
+
+@app.get("/", response_class=RedirectResponse)
+async def redirect():
+    return RedirectResponse(url="http://localhost:3000/home")
+
+api = overpy.Overpass()
+
+@app.get("/search_poi")
+async def search_poi(
+    q: str = Query(..., description="Query term for the POI, e.g., 'hospital'"),
+    lat: float = Query(..., description="Latitude of the search center"),
+    lon: float = Query(..., description="Longitude of the search center"),
+    radius: int = Query(1000, description="Radius in meters for the search"),
+    limit: int = Query(10, description="Limit the number of results")
+):
+    overpass_query = f"""
+    [out:json][timeout:25];
+    nwr(around:{radius},{lat},{lon})["amenity"="{q}"];
+    out center;
+    """
+    response = api.query(overpass_query)
+    result = []
+    for node in response.nodes:
+        name = node.tags.get("name", "n/a")
+        if(name != "n/a"):
+            result.append(name)
+
+    return result[:limit]
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Get port from environment variable or default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
